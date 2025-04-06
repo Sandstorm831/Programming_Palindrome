b8f7910e5f3cdf5005c7b016839e965793f68477
Sandstorm831 <gargrohan138@gmail.com>
2025-04-06 13:47:33 +0530

    added a more efficient solution to grid_unique_paths.py

--- striver_sheet/arrays_part_III/grid_unique_paths.py
+++ striver_sheet/arrays_part_III/grid_unique_paths.py
@@ -20,3 +20,17 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0 for x in range(n)] for y in range(m)]
         return recursor(m, n, m, n, dp)
+
+# Combinatorics solution: more efficient
+# we have n rows, so can take n-1 down steps and m columns, so can take m-1 right steps
+# so this question boild down to arranging the n-1 down and m-1 right steps
+# in an empty m+n-2 long empty string, it's equal to selecting m-1 or n-1 places, thus the solution.
+class ESolution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        N = m+n-2
+        r = m-1
+        res = 1
+        for i in range(1, r+1):
+            res *= (N-r+i)/i
+        res = round(res)
+        return int(res)
