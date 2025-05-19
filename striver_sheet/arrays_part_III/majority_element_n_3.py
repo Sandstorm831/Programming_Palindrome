c4ff382075f211ec3cf5e73740ca0ccce48113b5
Sandstorm831 <gargrohan138@gmail.com>
2025-05-19 15:52:02 +0530

    perfecting the majority_element_n_3 solution

--- striver_sheet/arrays_part_III/majority_element_n_3.py
+++ striver_sheet/arrays_part_III/majority_element_n_3.py
@@ -6,25 +6,25 @@ class Solution:
         maj1 = maj2 = c1 = c2 = 0
         for i in range(len(nums)):
             if nums[i] == maj1:
-                c1+=1
+                c1 += 1
             elif nums[i] == maj2:
-                c2+=1
+                c2 += 1
             elif c1 == 0:
                 maj1 = nums[i]
-                c1+=1
+                c1 += 1
             elif c2 == 0:
                 maj2 = nums[i]
-                c2+=1
+                c2 += 1
             else:
-                c1-=1
-                c2-=1
+                c1 -= 1
+                c2 -= 1
 
-        c1,c2 = 0, 0
+        c1, c2 = 0, 0
         for i in range(len(nums)):
             if nums[i] == maj1:
-                c1+=1
+                c1 += 1
             elif nums[i] == maj2:
-                c2+=1
+                c2 += 1
         res = []
         if c1 > len(nums)//3:
             res.append(maj1)
