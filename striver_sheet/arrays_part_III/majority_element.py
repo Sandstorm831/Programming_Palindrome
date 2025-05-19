36fcdbdc3587c93b0ef13b2013886c9e34c2fdce
Sandstorm831 <gargrohan138@gmail.com>
2025-05-19 15:52:26 +0530

    perfecting majority_element solution

--- striver_sheet/arrays_part_III/majority_element.py
+++ striver_sheet/arrays_part_III/majority_element.py
@@ -7,12 +7,11 @@ class Solution:
         freq = 1
         for i in range(1, len(nums)):
             temp = nums[i]
-            if nums[i] == holder :
-                freq+=1
+            if nums[i] == holder:
+                freq += 1
             elif nums[i] != holder:
                 freq -= 1
             if freq == 0:
                 holder = temp
                 freq = 1
         return holder
-            
